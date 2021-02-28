ae0b460896952daeb0e8c990bf72bb78c17a2b6c
Nate Reis <nathanlemos.ec@gmail.com>
2021-02-28 20:12:12 -0300

    Style

--- src/components/Fab.vue
+++ src/components/Fab.vue
@@ -14,22 +14,24 @@ export default {
 
 <style lang="scss" scoped>
     .fab {
-      background: $secondary;
+      background: $white;
       font-size: 48px;
       position: fixed;
       bottom: 45px;
       right: 45px;
       height: 60px;
       width: 60px;
+      border: solid 1px $secondary;
       border-radius: 50px;
       display: flex;
       justify-content: center;
       align-items: center;
-      color: white;
+      color: $secondary;
       cursor: pointer;
 
       &:hover {
         background-color: $primary;
+        color: $white;
       }
     }
 </style>
